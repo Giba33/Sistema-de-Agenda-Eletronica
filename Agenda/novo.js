88920f04da2c599685caec9733dbb585d5a9449c
Giba33 <61802819+Giba33@users.noreply.github.com>
2024-01-26 19:41:09 -0300

    Agenda.html
    Agenda.html

---
+++ Agenda/novo.js
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http=equiv="X-UA-Compatible" content="IE=chorme">
+    <meta name="viewport" content="width=device-width,initial-scale=1.o"> 
+    <title>Agenda Eletronica</title>
+</head>
+<body>
+
+<div  class="elementosForm">
+
+
+<div class="campoForm">
+<label>ID</label>
+<imput type="text" name="f_id" id="f_id">
+<div class="campoForm">
+        <label>Nome</label>
+        <imput type="text" name="f_nome" id="f_nome">
+        </div>
+
+
+        <div class="campoForm">
+            <label>celular</label>
+            <imput type="text" name="F_celular" id="f_celular">
+
+</div>
+<div class="campoForm">
+    <label>E-Mail</label>
+    <imput type="text" name="f_email" id="f_email">
+        </div>
+        <div class="campoForm">
+            <label>Data Nascimento</label>
+            <imput type="text" name="f_dtnasc" id="f_dtnasc">
+            </div>
+            <div class="botoesForm">
+            <button>Gravar</button>
+            <button>Cancelar</button>
+            </div>
+            </div>
+
+
+<script src="novo.js" type="modelo"></script>
+</body>
+</html>
