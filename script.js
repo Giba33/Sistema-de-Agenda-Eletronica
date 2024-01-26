f43c951e00d25b7725e253a1aa7626d1cec981ee
Giba33 <61802819+Giba33@users.noreply.github.com>
2024-01-26 14:08:47 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const cabecalho=document.querySelector("#cabecalho");
+const cabecalho=document.querySelector("#Home");
+const cabecalho=document.querySelector("#Nome");
+const cabecalho=document.querySelector("#telefone");
+const cabecalho=document.querySelector("#Novos");
+const cabecalho=document.querySelector("#Editar");
+const cabecalho=document.querySelector("#Adicionar");
+const cabecalho=document.querySelector("#Pesquisar");
+const cabecalho=documnent.querySelector("#Gestão");
+const cabecalho=document.querySelector("#Sobre")
+
+
